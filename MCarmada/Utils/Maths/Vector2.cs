1e18e84101f71be4f5d7e9f44232c9a9b532264f
Sean <sean@csnxs.uk>
2018-07-10 14:15:03 +0100

    Implement some stuff, at the demand of compiler warnings

--- MCarmada/Utils/Maths/Vector2.cs
+++ MCarmada/Utils/Maths/Vector2.cs
@@ -22,12 +22,27 @@ namespace MCarmada.Utils.Maths
 
         public static bool operator ==(Vector2 a, Vector2 b)
         {
-            return (a.X == b.X && a.Y == b.Y);
+            return a.Equals(b);
         }
 
         public static bool operator !=(Vector2 a, Vector2 b)
         {
-            return (a.X != b.X || a.Y != b.Y);
+            return !a.Equals(b);
+        }
+
+        public override bool Equals(object b)
+        {
+            if (b == null) return false;
+            if (b.GetType() != typeof(Vector2)) return false;
+            Vector2 bv = (Vector2) b;
+            return (X == bv.X && Y == bv.Y);
+        }
+
+        public override int GetHashCode()
+        {
+            int x = BitConverter.ToInt32(BitConverter.GetBytes(X), 0);
+            int y = BitConverter.ToInt32(BitConverter.GetBytes(Y), 0);
+            return x * y;
         }
     }
 }
--- MCarmada/Utils/Maths/Vector3.cs
+++ MCarmada/Utils/Maths/Vector3.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 
 namespace MCarmada.Utils.Maths
@@ -22,15 +23,30 @@ namespace MCarmada.Utils.Maths
             return "[" + X + ", " + Y + ", " + Z + "]";
         }
 
-
         public static bool operator ==(Vector3 a, Vector3 b)
         {
-            return (a.X == b.X && a.Y == b.Y && a.Z == b.Z);
+            return a.Equals(b);
         }
 
         public static bool operator !=(Vector3 a, Vector3 b)
         {
-            return (a.X != b.X || a.Y != b.Y || a.Z != b.Z);
+            return !a.Equals(b);
+        }
+
+        public override bool Equals(object b)
+        {
+            if (b == null) return false;
+            if (b.GetType() != typeof(Vector3)) return false;
+            Vector3 bv = (Vector3)b;
+            return (X == bv.X && Y == bv.Y && Z == bv.Z);
+        }
+
+        public override int GetHashCode()
+        {
+            int x = BitConverter.ToInt32(BitConverter.GetBytes(X), 0);
+            int y = BitConverter.ToInt32(BitConverter.GetBytes(Y), 0);
+            int z = BitConverter.ToInt32(BitConverter.GetBytes(Z), 0);
+            return x * y * z;
         }
     }
 }
